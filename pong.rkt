85a6fd18a8538ea45ed854ab0450e91bf9ec8459
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-08-22 05:23:40 +0800

    Add the constant definitions of the program

--- pong.rkt
+++ pong.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname pong) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
 (require 2htdp/image)
 (require 2htdp/universe)
 
@@ -6,6 +9,22 @@
 ;; =================
 ;; Constants:
 
+(define WINDOW-WIDTH 720)
+(define WINDOW-HEIGHT 1280)
+(define WINDOW-COLOR "Midnight Blue")
+(define WINDOW (empty-scene WINDOW-WIDTH WINDOW-HEIGHT WINDOW-COLOR))
+
+(define SHIELD-WIDTH 16)
+(define SHIELD-HEIGHT 96)
+(define SHIELD-COLOR "White")
+(define SHIELD-IMAGE (rectangle SHIELD-WIDTH SHIELD-HEIGHT "solid" SHIELD-COLOR))
+
+(define USER-SHIELD-XPOS 20)
+(define ENEMY-SHIELD-XPOS (- WINDOW-WIDTH 20))
+
+(define BALL-RADIUS 16)
+(define BALL-COLOR "White")
+(define BALL-IMAGE (circle BALL-RADIUS "solid" BALL-COLOR))
 
 
 ;; =================
